6fae42acd61a4f9b9492df182cc5e4ad977ce293
MasterMixGame <azizhonabdurahmanov2@gmail.com>
2024-07-17 15:24:44 +0500

    Splash page done

--- lib/Components/Constants.dart
+++ lib/Components/Constants.dart
@@ -1,3 +1,13 @@
 class AppImages{
-  static const Avto = "assets/images/Avtomobillar.png";
+  static const img1 = "assets/images/img1.png";
+  static const img2 = "assets/images/img2.png";
+  static const img3 = "assets/images/img3.png";
+  static const img4 = "assets/images/img4.png";
+  static const img5 = "assets/images/img5.jpg";
+  static const img6 = "assets/images/img6.jpg";
+  static const img7 = "assets/images/img7.png";
+  static const img8 = "assets/images/img8.jpg";
+  static const img9 = "assets/images/img9.png";
+  static const img10 = "assets/images/img10.png";
+  static const img11 = "assets/images/img11.jpg";
 }
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:uzum_market/Components/Constants.dart';
+import 'package:uzum_market/pages/main_page.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Future.delayed(Duration(seconds: 5)).then((value) =>
+        Navigator.pushAndRemoveUntil(context,
+            MaterialPageRoute(builder: (_) => MainPage()), (route) => false));
+    return Scaffold(
+      body: Center(
+          child: CircleAvatar(
+        backgroundColor: Color.fromARGB(255, 228, 213, 248),
+        radius: 105,
+        child: CircleAvatar(
+          backgroundImage: AssetImage(AppImages.img11),
+          radius: 100,
+        ),
+      )),
+    );
   }
 }
